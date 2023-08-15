5cb43c21d2f9ab50540beb2502316c5a0caf26af
CamMair <ubuntu@JWF-LTP27.jwf.local>
2023-08-15 16:01:03 +0100

    JDT-535 css alphabetised, cursors set to default, select set to none for date, day and month

--- src/components/Month.tsx
+++ src/components/Month.tsx
@@ -1,13 +1,13 @@
 const Day = (props: { className?: string; contents: string }) => {
-  return <div className={`${props.className || ''} col-1 inline-block h-6 w-6 text-sm`}>{props.contents}</div>;
+  return <div className={`${props.className || ''} col-1 h-6 inline-block text-sm w-6`}>{props.contents}</div>;
 };
 
 const DayNumbered = (props: { number: number }) => {
-  return <Day className="text-xs cursor-pointer hover:bg-gray-300 rounded-md justify-items-center pt-1 pb-1 pl-1.5 pr-1.5" contents={props.number.toString() } />;
+  return <Day className="cursor-pointer hover:bg-gray-300 justify-items-center rounded-md py-1 px-1.5 select-none text-xs " contents={props.number.toString() } />;
 };
 
 const DayOfWeekHeading = (props: { name: string }) => {
-  return <Day className="font-bold text-sm" contents={props.name} />;
+  return <Day className="cursor-default font-bold select-none text-sm" contents={props.name} />;
 };
 
 const DayPlaceholder = () => {
@@ -18,11 +18,11 @@ const Month = (props: { className?: string; index: number; title: string; year: 
   const daysInMonth = new Date(props.year, props.index + 1, 0).getDate();
   const dayNames = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
   const startDayOfMonth = new Date(props.year, props.index, 1).getDay();
-  const monthClasses = 'bg-white-500 text-black text-center flex flex-col items-center';
+  const monthClasses = 'bg-white-500 flex flex-col items-center text-black text-center';
 
   return (
     <div className={`${props.className} ${monthClasses}`}>
-      <div className="font-bold">{props.title}</div>
+      <div className="cursor-default font-bold select-none">{props.title}</div>
       <div className="grid grid-cols-7 mt-1">
         {dayNames.map(name => {
           return <DayOfWeekHeading name={name} />;
